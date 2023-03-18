a385c37d76b8879f166de51cbecbc5397529d1e3
Diego Haro <Diego Haro>
2023-03-17 21:01:58 -0300

    consulta al endpoint para traer pokemones

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-document.addEventListener("DOMContentLoaded", ()=>{ // espera a que la API responda
-    const geners = ["generation-1","generation-2","generation-3",
-                "generation-4", "generation-5", "generation-6", "generation-7"]
+const URL_API = `https://pokeapi.co/api/v2`
 
+document.addEventListener("DOMContentLoaded", ()=>{ // espera a que la API responda
+    
+    const fetchPokemons = async (endPoint) =>{
+        let data = ''
+        try {
+            const response = await fetch(endPoint, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            data = await response.json()
+            
+        } catch (error) {
+            console.log(error)
+            throw new Error(error)
+        }
+        return data.pokemon_species
+    }
+
+    
+    const getPokemons = async (num) =>{
+        let endPoint = `${URL_API}/generation/${num}/`
+        const container = document.getElementById("container")
+        container.innerHTML = ""
+        let pokemons = []
+        pokemons = await fetchPokemons(endPoint) // pokemones que me trae el endpoint
+        
+        pokemons.forEach((pokemon) =>{
+
+        })
+
+    }
+    // getPokemons(1)
+    
+    const geners = ["generation-1","generation-2","generation-3",
+    "generation-4", "generation-5", "generation-6", "generation-7"]
+    
     const filters = document.getElementById("filters")
     let gen = ""
     geners.forEach((generation, index) =>{
